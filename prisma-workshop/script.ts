fa9fb02f0667a2825fc9ad8eacd59bedd96e4b9f
TakayukiTooyama <t.tooyama107@gmail.com>
2022-02-21 20:22:11 +0900

    :sparkles: Task1: すべての User レコードを返すクエリの作成

--- prisma-workshop/script.ts
+++ prisma-workshop/script.ts
@@ -2,7 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const main = async () => {};
+const main = async () => {
+  // Task1: すべての User レコードを返すクエリの作成
+  const users = await prisma.user.findMany();
+  console.log(users);
+  // [
+  //   { id: 1, email: "takayuki@gmail.com", name: "taka" },
+  //   { id: 2, email: "akane@gmail.com", name: "akane" },
+  //   { id: 3, email: "tooyama@gmail.com", name: "tooyama" },
+  // ];
+};
 
 main()
   .catch((e) => {
